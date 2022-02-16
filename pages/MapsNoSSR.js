aa89e4e5c9054586ef8076ed2f6acb32c7198fb0
Amr Ashebo <amr8644@gmail.com>
2022-02-16 15:46:12 +0300

    fixed deployment issues

--- pages/MapsNoSSR.js
+++ pages/MapsNoSSR.js
@@ -32,8 +32,8 @@ const Maps = ({ results }) => {
       )}
 
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiYW1yODY0NCIsImEiOiJja3pqemhkcDkwMnJyMnZtbndlb2Y2NnVjIn0.rlFLYSYDyp5BgsWIA2o9cQ`}
+        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
       />
       {geoData.lat && geoData.lng && (
         <Marker position={[geoData.lat, geoData.lng]}>
