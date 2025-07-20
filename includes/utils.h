e4257c6881bbf6f5c8aab1850a1a5b757bc32f41
mertensblackfyre <72759681+mertensblackfyre@users.noreply.github.com.>
2025-07-20 23:59:09 +0300

    new

--- includes/utils.h
+++ includes/utils.h
@@ -6,31 +6,18 @@
 #include <iostream>
 #include <spdlog/spdlog.h>
 #include <vector>
+#include <algorithm>
 
 class Utils {
 
 public:
   inline void sort_files(std::vector<std::string> &pages);
   inline void get_files(std::vector<std::string> &pages, std::string path);
-  inline int extract_num(const std::string &fname);
+  inline static int extract_num(const std::string &fname);
 };
 
-int Utils::extract_num(const std::string &fname) {
-
-  int num;
-  size_t hyphen_pos = fname.find('-');
-  if (hyphen_pos == std::string::npos)
-    return 0;
-  try {
-    num = std::stoi(fname.substr(0, hyphen_pos));
-  } catch (std::exception error_) {
-    spdlog::error("{}", error_.what());
-    return -1;
-  }
-  return num;
-};
-
-void Utils::get_files(std::vector<std::string> &pages, std::string path) {
+inline void Utils::get_files(std::vector<std::string> &pages,
+                             std::string path) {
   struct dirent *dir;
   DIR *dp = opendir(path.c_str());
 
@@ -55,7 +42,7 @@ void Utils::get_files(std::vector<std::string> &pages, std::string path) {
   closedir(dp);
 };
 
-void Utils::sort_files(std::vector<std::string> &pages) {
+inline void Utils::sort_files(std::vector<std::string> &pages) {
 
   std::sort(pages.begin(), pages.end(),
             [](const std::string &lpage, const std::string &rpage) {
@@ -63,5 +50,20 @@ void Utils::sort_files(std::vector<std::string> &pages) {
               int rn = extract_num(rpage);
               return ln < rn;
             });
+};
+
+inline int Utils::extract_num(const std::string &fname) {
+
+  int num;
+  size_t hyphen_pos = fname.find('-');
+  if (hyphen_pos == std::string::npos)
+    return 0;
+  try {
+    num = std::stoi(fname.substr(0, hyphen_pos));
+  } catch (std::exception error_) {
+    spdlog::error("{}", error_.what());
+    return -1;
+  }
+  return num;
 };
 #endif
--- main.cc
+++ main.cc
@@ -1,71 +1,20 @@
-#include <algorithm>
-#include <dirent.h>
-#include <hpdf.h>
+#include "includes/mangashu.h"
+#include "includes/utils.h"
 #include <iostream>
-#include <spdlog/spdlog.h>
 #include <vector>
 
-void mangahu_chapter(std::string path,const std::vector<std::string> &pages);
 int main() {
   std::vector<std::string> pages;
- // get_files(pages, "ss");
 
-  mangahu_chapter("ss",pages);
-  // for(auto h : pages){
-  //   std::cout << h << std::endl;
-  //  }
+  Utils utils;
+  MangaShu main;
+  std::string d_name = "ss";
+
+  utils.get_files(pages, d_name);
+  main.mangashu_chapter(d_name, pages);
+
+  //  for(auto h : pages){
+  //    std::cout << h << std::endl;
+  //   }
   return 0;
 }
-
-void mangashu_chapter(std::string path,const std::vector<std::string> &pages) {
-
-  if (pages.empty()) {
-    spdlog::warn("No images provided.");
-    return;
-  };
-
-  HPDF_Doc pdf = HPDF_New(nullptr, nullptr);
-  if (!pdf) {
-    spdlog::error("Failed to create PDF object.");
-    return;
-  }
-
-  for (const auto &path : pages) {
-    // Load image file (e.g., "image.jpg")
-    std::string ppp = "ss/" + path;
-    HPDF_Image image = HPDF_LoadJpegImageFromFile(pdf, ppp.c_str());
-    if (!image) {
-      spdlog::error("Failed to load image: {}", path);
-      continue;
-    };
-
-    HPDF_Page page = HPDF_AddPage(pdf);
-    HPDF_Page_SetSize(page, HPDF_PAGE_SIZE_A4, HPDF_PAGE_PORTRAIT);
-
-    HPDF_REAL page_width = HPDF_Page_GetWidth(page);
-    HPDF_REAL page_height = HPDF_Page_GetHeight(page);
-
-    HPDF_REAL img_width = HPDF_Image_GetWidth(image);
-    HPDF_REAL img_height = HPDF_Image_GetHeight(image);
-
-    HPDF_REAL scale =
-        std::min(page_width / img_width, page_height / img_height);
-
-    HPDF_REAL draw_width = img_width * scale;
-    HPDF_REAL draw_height = img_height * scale;
-
-    // Center the image on the page
-    HPDF_REAL x = (page_width - draw_width) / 2;
-    HPDF_REAL y = (page_height - draw_height) / 2;
-
-    HPDF_Page_DrawImage(page, image, x, y, draw_width, draw_height);
-  }
-
-  HPDF_STATUS result = HPDF_SaveToFile(pdf, "output.pdf");
-  if (result != HPDF_OK) {
-    spdlog::error("Failed to save output.pdf");
-  } else {
-    spdlog::info("PDF successfully saved to output.pdf");
-  }
-  return;
-};
